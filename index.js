8af1a6900a85923c4934b203932df31162aec359
FowziaSultana <fowziasultana168@gmail.com>
2023-05-02 20:20:37 +0600

    added chefs.json and index.json

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const chefs = require("./data/Chefs.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Dragon is running");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.listen(port, () => {
+  console.log(`Dragon API is running on port: ${port}`);
+});
