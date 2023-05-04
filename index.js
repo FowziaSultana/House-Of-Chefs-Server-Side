365906cffa9c0ef2c357895e84e742943fdc2cd3
FowziaSultana <fowziasultana168@gmail.com>
2023-05-04 15:47:40 +0600

    updated index.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const chefs = require("./data/Chefs.json");
+const recipes = require("./data/Recipes.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Dragon is running");
+  res.send("App is running");
 });
 
 app.get("/chefs", (req, res) => {
@@ -17,10 +18,18 @@ app.get("/chefs", (req, res) => {
 app.get("/chefs/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const selectedChef = chefs.find((chef) => chef.ChefId === id);
-
   res.send(selectedChef);
+});
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedRecipes = recipes.filter((rec) => rec.chef_id === id);
+  res.send(selectedRecipes);
 });
 
 app.listen(port, () => {
-  console.log(`Dragon API is running on port: ${port}`);
+  console.log(`Chefs API is running on port: ${port}`);
 });
